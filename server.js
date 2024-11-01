b70ec5f6cbaf654f073640ab04c80c1d82ed0932
Baharpa <bparsaeian@myseneca.ca>
2024-10-31 21:07:56 -0400

    last change

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-/*WEB322 – Assignment 02 & 3
+/*WEB322 – Assignment 3 & 2
 I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part * of this assignment has
 been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 Name: Bahar Parsaeian
 Student ID: 118314210
 Date: 2024-10-09
-Render Web App URL:    https://web322-app-bohj.onrender.com
+Render Web App URL:    https://web322-app-bohj.onrender.com/about
 GitHub Repository URL: https://github.com/Baharpa/web322-app
 SSH:                   git@github.com:Baharpa/web322-app.git
 ********************************************************************************/
